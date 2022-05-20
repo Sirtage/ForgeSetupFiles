3b86e919a4319e7feab24d9ef59bca59713d1b00
Sirtage <88209256+Sirtage@users.noreply.github.com>
2022-05-21 01:40:32 +0300

    v1.1
    using randomly mb

--- Register.java
+++ Register.java
@@ -3,6 +3,7 @@ package net.sirtage.reg;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ToolItem;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.util.ResourceLocation;
@@ -14,27 +15,51 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.sirtage.SomeTest;
 
 import net.sirtage.content.Tabs;
+import net.sirtage.content.blocks.BlackShit;
+import net.sirtage.content.blocks.TestBlock;
+import net.sirtage.content.items.HelloNewContent;
+import net.sirtage.content.items.ShitRemover;
+import net.sirtage.content.items.idfk;
+import net.sirtage.content.items.sstaff;
 
 import java.util.function.Supplier;
 
 public class Register {
+    public static class content {
 
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SomeTest.MOD_ID);
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, SomeTest.MOD_ID);
+        public static final DeferredRegister<Item> items = DeferredRegister.create(ForgeRegistries.ITEMS, SomeTest.MOD_ID);
+        public static final DeferredRegister<Block> blocks = DeferredRegister.create(ForgeRegistries.BLOCKS, SomeTest.MOD_ID);
 
-    public static void __init__() {
-        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        //Items
+        public static final RegistryObject<Item> IDFK = items.register("idfk", idfk::new);
+        public static final RegistryObject<Item> HNW = items.register("helnewcon", HelloNewContent::new);
+        public static final RegistryObject<Item> SHIT_REMOVER = items.register("shit_remover", ShitRemover::new);
+
+        //Tools
+        public static final RegistryObject<ToolItem> SWAND = items.register("sstaff", sstaff::new);
+
+        //Blocks
+        public static final RegistryObject<Block> BSHIT = Register.registryBlock("blackshit", BlackShit::new);
+        public static final RegistryObject<Block> TESTBLOCK = Register.registryBlock("testblock", TestBlock::new);
+
+        //Tags
+        public static final Tags.IOptionalNamedTag<Block> IS_SHIT = Register.registerBlockTag(SomeTest.MOD_ID, "is_shit");
     }
 
-    public static <T extends Block>RegistryObject<T> registryBlock(String name, Supplier<T> block) {
-        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+
+    public static void __init__() {
+        content.items.register(FMLJavaModLoadingContext.get().getModEventBus());
+        content.blocks.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    public static <T extends Block> RegistryObject<T> registryBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = content.blocks.register(name, block);
         registryBlockItem(name, toReturn);
         return toReturn;
     }
 
     public static <T extends Block> void registryBlockItem(String name, RegistryObject<T> block) {
-        ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().group(Tabs.SirMainTab)));
+        content.items.register(name, () -> new BlockItem(block.get(), new Item.Properties().group(Tabs.SirMainTab)));
     }
 
     public static Tags.IOptionalNamedTag<Item> registerItemTag(String namespace, String tagName) {
